1722538c46a096d18af040894d4adee3878ffeb4
Damain Trowski <damian.dt.pl@gmail.com>
2024-10-15 23:48:50 +0200

    Created autoSave script
    Adding new script, appending old autoCopy

--- autoCopy.bat
+++ autoCopy.bat
@@ -12,6 +12,10 @@ docker exec prestashop sh -c "cp -r /tmp/project-prestashop/html/* /var/www/html
 
 docker exec prestashop rm -rf /tmp/project-prestashop
 
+docker exec prestashop rm -rf /var/www/html/install
+
+docker exec prestashop rm -rf /var/www/html/admin
+
 
 docker exec prestashop echo "html content:"
 
---
+++ autoSave.bat
@@ -0,0 +1,8 @@
+@echo off
+
+docker-compose up -d
+
+docker cp prestashop:/var/www/html ./prestashop
+
+pause
+
