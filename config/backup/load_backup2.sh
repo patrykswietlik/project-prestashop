f7174252b0cb18fd656711a447115c71faf51c12
Patryk Świetlik <114610438+patrykswietlik@users.noreply.github.com>
2025-01-16 17:22:27 +0100

    Update load_backup2.sh

--- config/backup/load_backup2.sh
+++ config/backup/load_backup2.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 echo "Restoring a database from a file ./backup/prestashop_db_backup.sql" 
-mysql -h localhost -u root -pprestashop prestashop < "./backup/prestashop_db_backup.sql" 
+mysql -h 98.82.14.176 -u root -pprestashop prestashop < "./backup/prestashop_db_backup.sql" 
 echo "end"
 
