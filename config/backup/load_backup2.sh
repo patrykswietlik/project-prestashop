288f54a3f6899de59c4ebb392bd2ef5884d9cec6
Patryk Świetlik <114610438+patrykswietlik@users.noreply.github.com>
2025-01-16 21:59:38 +0100

    Update load_backup2.sh

--- config/backup/load_backup2.sh
+++ config/backup/load_backup2.sh
@@ -9,5 +9,31 @@ echo "password: ${DB_PASSWD}. "
 
 mysql -h $DB_SERVER -u $DB_USER -p$DB_PASSWD $DB_NAME < "./backup/prestashop_db_backup.sql" 
 
+echo "<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => '${DB_SERVER}',
+    'database_port' => '',
+    'database_name' => '${DB_NAME}',
+    'database_user' => '${DB_USER}',
+    'database_password' => '${DB_PASSWD}',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'L77j02yu7eG8hr4DKAMnBNRM5mejcsn9ggoqsKEKjmmHtvweo2WZfsuK5u1tRqug',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2024-12-08',
+    'locale' => 'en-US',
+    'use_debug_toolbar' => true,
+    'cookie_key' => '7fRLDmNMIFjrQhhgK4QK04OacZut1fomguptb4YmrtFosQLOZZQbeq4Wd2NTzVvq',
+    'cookie_iv' => 'IMyO4aphsKOTK2x0D1oBymmZKb9BHyk6',
+    'new_cookie_key' => 'def00000250407408e5a3b0111fe0ffb21fc56831c7b3b8d225b1b8676f99e63fb24987c285129ad816bdba3feef7ab51314165bd3738d5e349e9d00bbae7bb491a53ebf',
+  ),
+);" > /var/www/html/app/config/parameters.php
+
 echo "end"
 
